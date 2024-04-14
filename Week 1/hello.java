208e863e97f572c4ef74252ae51fa14bfdecc7f1
rijan <rijanregmi111@gmail.com>
2024-04-14 12:17:31 +0545

    Week 1 comment

--- Week 1/hello.java
+++ Week 1/hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args) {
+        /*
+         * 1. Write a program that prints Hello World! to the console. Save the program
+         * as Hello.java.
+         */
         System.out.println("Hello World");
     }
 }
